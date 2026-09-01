d04a524549f840d88b24a2930b2377cdc13dcea2
gxickx <mgrokoe@gmail.com>
2026-09-01 12:37:05 -0300

    Excepciones para el modulo de agenda de usuario

---
+++ src/main/java/org/mgroko/backend/calendario/exception/BloqueoSolapadoException.java
@@ -0,0 +1,7 @@
+package org.mgroko.backend.calendario.exception;
+
+public class BloqueoSolapadoException extends RuntimeException {
+    public BloqueoSolapadoException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/mgroko/backend/calendario/exception/RangoInvalidoException.java
@@ -0,0 +1,7 @@
+package org.mgroko.backend.calendario.exception;
+
+public class RangoInvalidoException extends RuntimeException {
+    public RangoInvalidoException(String message) {
+        super(message);
+    }
+}
